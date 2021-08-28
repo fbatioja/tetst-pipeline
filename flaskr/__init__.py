6f92aaf8a4dc9b9b7a66d22f9b34a26808c96b88
fbatioja <f.batioja@uniandes.edu.co>
2021-08-28 15:27:50 -0500

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,4 +6,5 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY']='frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
+    
     return app
