d2cf531b6ace26c0f73d672bdf5accc89cf95d99
fbatioja <f.batioja@uniandes.edu.co>
2021-08-28 23:37:11 -0500

    Update app.component.spec.ts

--- front-end/tutorial-canciones/src/app/app.component.spec.ts
+++ front-end/tutorial-canciones/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'testPipeline'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testPipeline');
+    expect(app.title).toEqual('testPipeline111');
   });
 
   it('should render title', () => {
